c132dacd49947f07fc7ebaa2df29061c64be4937
Ranula Ranatunga <63148540+RanulaRanatunga@users.noreply.github.com>
2020-12-11 13:06:22 +0530

    Update ForgotPasswordVC.swift

--- FirebaseAuthTest/VC/ForgotPasswordVC.swift
+++ FirebaseAuthTest/VC/ForgotPasswordVC.swift
@@ -2,8 +2,8 @@
 //  ForgotPasswordVC.swift
 //  FirebaseAuthTest
 //
-//  Created by Gautham Sritharan on 6/25/20.
-//  Copyright © 2020 Hashan Kannangara. All rights reserved.
+//  Created by Ranula Ranatunga  on 6/25/20.
+//  Copyright © 2020 Ranula Ranatunga . All rights reserved.
 //
 
 import UIKit
