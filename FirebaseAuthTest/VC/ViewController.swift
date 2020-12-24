fb32121a8b21770063214c17a2fd8637cec71b57
Ranula Ranatunga <63148540+RanulaRanatunga@users.noreply.github.com>
2020-12-24 10:23:42 +0530

    Update ViewController.swift

--- FirebaseAuthTest/VC/ViewController.swift
+++ FirebaseAuthTest/VC/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  FirebaseAuthTest
 //
-//  Created by Ranula Ranatung on 6/25/20.
-//  Copyright © 2020 Ranula Ranatung. All rights reserved.
+//  Created by Ranula Ranatunga on 6/25/20.
+//  Copyright © 2020 Ranula Ranatunga. All rights reserved.
 //
 
 import UIKit
