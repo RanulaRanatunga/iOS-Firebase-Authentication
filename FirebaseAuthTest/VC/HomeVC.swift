301cf5db182c17f8169fe92bb24871a530cf04b3
Ranula Ranatunga <63148540+RanulaRanatunga@users.noreply.github.com>
2020-12-24 10:22:39 +0530

    Update HomeVC.swift

--- FirebaseAuthTest/VC/HomeVC.swift
+++ FirebaseAuthTest/VC/HomeVC.swift
@@ -2,8 +2,8 @@
 //  HomeVC.swift
 //  FirebaseAuthTest
 //
-//  Created by Gautham Sritharan on 6/25/20.
-//  Copyright © 2020 Hashan Kannangara. All rights reserved.
+//  Created by Ranula Ranatung on 6/25/20.
+//  Copyright © 2020 Ranula Ranatunga. All rights reserved.
 //
 
 import UIKit
