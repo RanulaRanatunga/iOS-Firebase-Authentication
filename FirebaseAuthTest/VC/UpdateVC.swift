b4d8bacbc777d97236e397d80d415fe66df93fbe
Ranula Ranatunga <63148540+RanulaRanatunga@users.noreply.github.com>
2020-12-24 10:24:14 +0530

    Update UpdateVC.swift

--- FirebaseAuthTest/VC/UpdateVC.swift
+++ FirebaseAuthTest/VC/UpdateVC.swift
@@ -2,8 +2,8 @@
 //  UpdateVC.swift
 //  FirebaseAuthTest
 //
-//  Created by Gautham Sritharan on 6/29/20.
-//  Copyright © 2020 Hashan Kannangara. All rights reserved.
+//  Created by Ranula Ranatunga on 6/29/20.
+//  Copyright © 2020 Ranula Ranatunga. All rights reserved.
 //
 
 import UIKit
